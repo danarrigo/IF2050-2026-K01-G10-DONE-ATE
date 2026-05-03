5fecce4b5290c117cd5353ed2d17783415983cbd
Daniel Arrigo <arrigo1888@gmail.com>
2026-05-03 15:58:40 +0700

    feat: created UI abstract class

---
+++ src/main/java/boundaries/UI.java
@@ -0,0 +1,33 @@
+package boundaries;
+
+import entities.Donation;
+import entities.User;
+
+import java.util.List;
+
+public abstract class UI {
+    private List<Donation> donations;
+    private User user;
+
+    public UI(User user) {
+        this.user = user;
+    }
+
+    public List<Donation> getDonations() {
+        return donations;
+    }
+
+    public void setDonations(List<Donation> donations) {
+        this.donations = donations;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public abstract void showUI();
+}
