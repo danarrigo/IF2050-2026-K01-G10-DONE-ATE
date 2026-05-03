eaa0f5230df2f6ab6be970dd874908147422ac02
Daniel Arrigo <arrigo1888@gmail.com>
2026-05-03 14:07:08 +0700

    feat: define User entity and JPA mapping

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,81 @@
+package entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
+    @Id
+    private String username;
+    private String password;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    @ElementCollection
+    private List<String> notificationList;
+
+
+    public User(String username, String password, String address, String phoneNumber, String email, List<String> notificationList) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.notificationList = notificationList;
+    }
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getNotificationList() {
+        return notificationList;
+    }
+
+    public void setNotificationList(List<String> notificationList) {
+        this.notificationList = notificationList;
+    }
+}
