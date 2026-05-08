28d49066b91141a58cbf521291ef0ac5470b7785
Daniel Arrigo <arrigo1888@gmail.com>
2026-05-08 18:13:01 +0700

    ci: add H2 test database to fix GitHub Actions build

--- src/test/java/io/github/danarrigo/if20502026k01g1doneate/If2050ApplicationTests.java
+++ src/test/java/io/github/danarrigo/if20502026k01g1doneate/If2050ApplicationTests.java
@@ -3,7 +3,10 @@ package io.github.danarrigo.if20502026k01g1doneate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class If2050ApplicationTests {
 
     @Test
