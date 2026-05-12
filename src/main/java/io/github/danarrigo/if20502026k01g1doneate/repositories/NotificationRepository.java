5f02dfcb8280504fd93106926f58384ea31e3732
LaurensiusDani <dani.rendragraha@gmail.com>
2026-05-12 14:37:01 +0700

    feat: create NotificationRepository

---
+++ src/main/java/io/github/danarrigo/if20502026k01g1doneate/repositories/NotificationRepository.java
@@ -0,0 +1,14 @@
+package io.github.danarrigo.if20502026k01g1doneate.repositories;
+
+import io.github.danarrigo.if20502026k01g1doneate.entities.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    
+    List<Notification> findByUser_UsernameOrderByTimeStampDesc(String username);
+}
